9aa0c48504cd472f5a2b488c88543488e21c27dc
Bander Alfaifi <67489440+BanderAlfaifi@users.noreply.github.com>
2024-09-19 22:22:23 +0300

    Finshing Task 1,2, and 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,18 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// A function for searching the books based on the kay and value
+function searchBooks(key, value) {
+  let results = [];
+  
+  for (let i in books) {
+    if (books[i][key] && books[i][key].toLowerCase() === value.toLowerCase()) {
+      results.push(books[i]);
+    }
+  }
+  
+  return results;
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,20 +24,21 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200).json(books)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  const book = books[isbn]
+  res.status(200).json(book)
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const book = searchBooks("author", author);
+  res.status(200).json(book)
 });
 
 // Get all books based on title
